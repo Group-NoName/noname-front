de286b01ddd0c8c339db119fe7b42527edbc1dbd
ViniciusAndBar <vini.zeus2002@gmail.com>
2022-09-13 22:48:46 -0300

    Adição de Rotas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,11 @@ import Produto from '../pages/Produto';
 import HomeProduto from '../pages/Admin/Produtos/Home'
 import CadastroProduto from '../pages/Admin/Produtos/Cadastro'
 import HomeAdmin from '../pages/Admin/Home/index';
+import VisualizarProduto from '../pages/Admin/Produtos/Visualizar'
+import EditarProduto from '../pages/Admin/Produtos/Editar'
+import CadastroCategoria from '../pages/Admin/Categorias/Cadastro'
+import HomeCategoria from '../pages/Admin/Categorias/Home'
+import EditarCategoria from '../pages/Admin/Categorias/Editar'
 
 function Routes() {
     return (
@@ -15,7 +20,14 @@ function Routes() {
           <Route index element={<HomeAdmin/>}/> 
           <Route path="produtos">
             <Route index element={ <HomeProduto/> } />
-            <Route path="cadastro" element={<CadastroProduto/>} /> 
+            <Route path="cadastro" element={<CadastroProduto/>} />
+            <Route path="visualizar/:id" element={<VisualizarProduto/>} />  
+            <Route path="editar/:id" element={<EditarProduto/>} />
+          </Route>
+          <Route path="categorias">
+            <Route index element={ <HomeCategoria/> } />
+            <Route path="cadastro" element={<CadastroCategoria/>} />       
+            <Route path="editar/:id" element={<EditarCategoria/>} />
           </Route>
         </Route>
       </RoutesWrapper>
