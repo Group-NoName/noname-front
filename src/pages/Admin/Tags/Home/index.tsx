242abb65c2c3f9fcf0421190881d9cf8eb0508c3
ZeroPirata <gabriel0gsbn@gmail.com>
2022-09-30 23:10:38 -0300

    Search Bar tags

--- src/pages/Admin/Tags/Home/index.tsx
+++ src/pages/Admin/Tags/Home/index.tsx
@@ -4,17 +4,19 @@ import { api } from '../../../../service/api';
 import tags from "../../../../interfaces/tags"
 import * as S from './styles'
 import Nav_Admin from '../../../../components/Nav_Admin';
-import { Button, Table } from 'react-bootstrap';
+import { Button, Form, Table } from 'react-bootstrap';
 
 
 function homeTag() {
+
     const [tags, setTags] = useState<tags[]>([])
+    const [tag, searchTag] = useState<tags[]>([])
+    const [searchInput, setSearchInput] = useState('');
+    const [filteredResults, setFilteredResults] = useState<tags[]>([]);
+
     const navigate = useNavigate();
     useEffect(() => { getTags() });
-    async function getTags() {
-        const response = await api.get<tags[]>('/tag/tags')
-        setTags(response.data)
-    }
+
     const deleteTags = useCallback(
         async (id: string) => {
             await api.delete(`/tag/excluir/${id}`)
@@ -25,9 +27,30 @@ function homeTag() {
                 })
         }, []
     )
+
+    async function getTags() {
+        const response = await api.get<tags[]>('/tag/tags')
+        setTags(response.data)
+    }
+
+    const searchItems = (searchValue: any) => {
+        setSearchInput(searchValue)
+        const filteredData = tag?.filter((item) => {
+            return Object.values(item.nome).join('').toLowerCase().includes(searchInput.toLowerCase())
+        })
+        setFilteredResults(filteredData)
+    }
+
+    useEffect(() => {
+        api.get(`/tag/tags`)
+            .then((response) => {
+                searchTag(response.data);
+            })
+    }, [])
+
     return (
         <section>
-            <Nav_Admin/>
+            <Nav_Admin />
             <S.Home>
                 <main>
                     <div className="Form">
@@ -39,15 +62,45 @@ function homeTag() {
                                 </tr>
                             </thead>
                             <tbody>
-                                {tags && tags.map(i => {
+                                <Form.Control aria-label="Text input with dropdown button"
+                                    onChange={(e) => searchItems(e.target.value)}
+                                    placeholder="Buscar Tag" />
+                                {searchInput.length > 1 ? (
+                                    filteredResults.map((item) => {
+                                        return (
+                                            <tr key={item.id}>
+                                                <td>{item.nome}</td>
+                                                <td className="tdbuttons">
+                                                    <div className="buttons">
+                                                        <Button variant="outline-primary" onClick={() => navigate(`/admin/tags/editar/${item.id}`)}>Editar</Button>{' '}
+                                                        <Button variant="outline-success" onClick={() => navigate(`/admin/tags/visualizar/${item.id}`)}>Visualizar</Button>
+                                                        <Button variant="outline-danger" onClick={() => deleteTags(item.id)}>Deletar</Button>{' '}
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        )
+                                    })
+                                ) : tags && tags.map(i => {
                                     return (
                                         <tr key={i.id}>
                                             <td>{i.nome}</td>
                                             <td className="tdbuttons">
                                                 <div className="buttons">
-                                                    <Button variant="outline-primary" onClick={() => navigate(`/admin/tags/editar/${i.id}`)}>Editar</Button>{' '}
-                                                    <Button variant="outline-success" onClick={() => navigate(`/admin/tags/visualizar/${i.id}`)}>Visualizar</Button>
-                                                    <Button variant="outline-danger" onClick={() => deleteTags(i.id)}>Deletar</Button>{' '}
+                                                    <Button variant="outline-primary"
+                                                        onClick={() => navigate(`/admin/tags/editar/${i.id}`)}
+                                                    >
+                                                        Editar
+                                                    </Button>{' '}
+                                                    <Button variant="outline-success"
+                                                        onClick={() => navigate(`/admin/tags/visualizar/${i.id}`)}
+                                                    >
+                                                        Visualizar
+                                                    </Button>
+                                                    <Button variant="outline-danger"
+                                                        onClick={() => deleteTags(i.id)}
+                                                    >
+                                                        Deletar
+                                                    </Button>{' '}
                                                 </div>
                                             </td>
                                         </tr>
