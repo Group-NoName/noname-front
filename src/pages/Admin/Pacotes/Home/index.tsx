a576db80231f5329563a157c433d8f62619135aa
ViniciusAndBar <vini.zeus2002@gmail.com>
2022-10-03 20:56:26 -0300

    Criação e integração da tela Home pacotes

--- src/pages/Admin/Pacotes/Home/index.tsx
+++ src/pages/Admin/Pacotes/Home/index.tsx
@@ -1,15 +1,92 @@
 import * as S from './styles';
+import { useCallback, useEffect, useState } from 'react';
 import Nav_Admin from "../../../../components/Nav_Admin"
 import { api } from "../../../../service/api"
+import { Button, Form, Table } from 'react-bootstrap';
+import Ipacote from '../../../../interfaces/pacote'
 
 function home() {
-  return(
-    <section>
-      <Nav_Admin/>
-      <S.Home>
-        <h1>Ho me</h1>
-      </S.Home>
-    </section>
-  ) 
+    const [pacotes, setPacotes] = useState<Ipacote[]>([])
+    const [pacote, searchPacote] = useState<Ipacote[]>([])
+    const [searchInput, setSearchInput] = useState('');
+    const [filteredResults, setFilteredResults] = useState<Ipacote[]>([]);
+
+
+    const searchItems = (searchValue: any) => {
+        setSearchInput(searchValue)
+        const filteredData = pacote?.filter((item) => {
+            return Object.values(item.nome).join('').toLowerCase().includes(searchInput.toLowerCase())
+        })
+        setFilteredResults(filteredData)
+    }
+
+    useEffect(() => {
+        api.get(`/pacote/pacotes`)
+            .then((response) => {
+                searchPacote(response.data);
+            })
+    }, [])
+
+    useEffect(() => { getPacotes() })
+    async function getPacotes() {
+        const response = await api.get<Ipacote[]>('/pacote/pacotes')
+        setPacotes(response.data)
+    }
+    return (
+        <section>
+            <Nav_Admin />
+            <S.Home>
+                <main>
+                    <div className="Form">
+                        <Table striped bordered hover>
+                            <thead>
+                                <tr>
+                                    <th>Nome</th>
+                                    <th>Preço</th>
+                                    <th>Ações</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <Form.Control aria-label="Text input with dropdown button"
+                                    onChange={(e) => searchItems(e.target.value)}
+                                    placeholder="Buscar pacote" />
+                                {searchInput.length > 1 ? (
+                                    filteredResults.map((pacotes) => {
+                                        return (
+                                            <tr>
+                                                <td>{pacotes.nome}</td>
+                                                <td>{pacotes.preco}</td>
+                                                <td className="tdbuttons">
+                                                    <div className="buttons">
+                                                        <Button variant="outline-primary">Editar</Button>{' '}
+                                                        <Button variant="outline-success">Visualizar</Button>
+                                                        <Button variant="outline-danger">Deletar</Button>{' '}
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        )
+                                    })
+                                ) : pacotes && pacotes.map((pacotes) => {
+                                    return (
+                                        <tr>
+                                            <td>{pacotes.nome}</td>
+                                            <td>{pacotes.preco}</td>
+                                            <td className="tdbuttons">
+                                                <div className="buttons">
+                                                    <Button variant="outline-primary">Editar</Button>{' '}
+                                                    <Button variant="outline-success">Visualizar</Button>
+                                                    <Button variant="outline-danger">Deletar</Button>{' '}
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    )
+                                })}
+                            </tbody>
+                        </Table>
+                    </div>
+                </main>
+            </S.Home>
+        </section>
+    )
 }
 export default home;
--- src/pages/Admin/Pacotes/Home/styles.ts
+++ src/pages/Admin/Pacotes/Home/styles.ts
@@ -1,3 +1,25 @@
 import styled from "@emotion/styled";
 
-export const Home = styled.div``
+export const Home = styled.div`
+
+main{
+    display: flex;
+}
+.Form{
+    margin: 45px auto;
+    width: 50vw;
+    display: flex;
+    
+    Table{
+        margin: 0 auto auto;
+        width: 100%;
+        .tdbuttons{
+            .buttons{
+                display: flex;
+                gap: 5px;
+            }
+        }
+    }
+}
+
+`
