aec61b0da859e1cfc3b8faf10f5c27d7c3b7c030
ViniciusAndBar <vini.zeus2002@gmail.com>
2022-10-03 20:59:31 -0300

    Criação e integração da tela do Pacote unico (visualização do cliente)

--- src/pages/Pacote/index.tsx
+++ src/pages/Pacote/index.tsx
@@ -1,8 +1,87 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Button from '../../components/Button';
+import CardProds from '../../components/CardProds';
+import Nav_ from '../../components/Nav';
+import Ipacote from '../../interfaces/pacote';
+import { api } from '../../service/api';
 import * as S from './styles';
 
 function Pacote() {
-  return(
-    <h1>Pac Ote</h1>
+  const { id } = useParams();
+  const [pacote, getPacote] = useState<Ipacote>();
+  useEffect(() => { getPacotes() }, [id]);
+  async function getPacotes() {
+    const response = await api.get<Ipacote>(`/pacote/pacote/${id}`)
+    getPacote(response.data)
+  }
+  return (
+    <>
+      <Nav_ />
+      <S.Container>
+        <div className='pacoteContent'>
+          <div className='imgLateral'>
+            {pacote?.images.map((i) => {
+              return (
+                <>
+                  <div className="imgsLateral">
+                    <div className="imgs">
+                      <img className='tamanho' src={`${i.url}`} alt="" />
+                    </div>
+                  </div>
+                </>)
+            })}
+          </div>
+          <div className='imgCentral'>
+            <div className="posicao">
+              <img className='tamanho' src={pacote?.images[0].url} alt="" />
+            </div>
+          </div>
+          <div className='pacoteInformation'>
+            <div className='pacoteDescricao'>
+              <div className="nome">
+                <h1>{pacote?.nome}</h1>
+              </div>
+              <div className="desc">
+                <p>{pacote?.descricao}</p>
+                <h3>Itens inclusos</h3>
+                {pacote?.produtos.map(itens => {
+                  return (
+                    <a href="">{itens.nome}<br /></a>
+                  )
+                })}
+              </div>
+              <div className='pacotePrice'>
+                <div className='price'>
+                  <h2>R$ {pacote?.preco}</h2>
+                </div>
+                <div className='pacotepricebuttom'>
+                  <Button color={'#ffff'} width={'8'} height={'3'} fontSize={'20'} backgroundColor={'#3a4ad9'} text={'Comprar'} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='pacotesLists'>
+          <div className='produtosSemelhantes'>
+            <h2>Produtos Semelhantes</h2>
+            <div className="cards">
+              <div className="card">
+                <CardProds name={''} imageURL={''} produtoID={''} preco={0} />
+              </div>
+            </div>
+          </div>
+          <div className='produtosRelacionados'>
+            <h2>Produtos Relacionados</h2>
+            <div className='posicao'>
+              <div>
+                <CardProds name={''} imageURL={''} produtoID={''} preco={0} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </S.Container>
+    </>
   )
 }
 export default Pacote;
--- src/pages/Pacote/styles.ts
+++ src/pages/Pacote/styles.ts
@@ -1,3 +1,210 @@
 import styled from '@emotion/styled';
 
-export const Container = styled.div``
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  overflow-x: hidden;
+  min-height: 89vh;
+  margin-top: 0.5em;
+
+  .pacoteContent{
+    width: 90vw;
+    height: 55vh;
+    display: flex;
+    
+    .imgLateral{ 
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      gap: 15px;
+      width: 15%;
+
+      .imgsLateral{
+        text-align: center;
+        width: 150px;
+        height: 110px;
+        border: 1px solid pink;
+        border-radius: 20px;
+        overflow: hidden;
+        
+        .imgs{
+          display: flex;
+          justify-content: center;
+          width: 100%;
+          height: 100%;
+          .tamanho{
+            object-fit: cover;
+          }
+        }
+      }
+    }
+    .imgCentral{ 
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 40%;
+        .posicao{
+          overflow: hidden;
+          display: flex;
+          justify-content: center;
+          width: 80%;
+          height: 80%;
+          text-align: center;
+          border: 1px solid pink;
+          border-radius: 20px;
+
+          
+          .tamanho{
+            object-fit: cover;
+          }
+        }
+    }
+    .pacoteInformation{
+        width: 45%;
+        border: 1px solid pink;
+        border-radius: 20px;
+
+        .pacoteDescricao{
+          height: 80%;
+          
+          .nome{
+            width: 100%;
+            height: 25%;
+            border-bottom: 1px solid pink;
+            h1{
+              padding: 1vw;
+              font-size: 100%;
+            }
+          }
+          .desc{
+            width: 100%;
+            height: 70%;
+            margin: auto;
+            border-bottom: 1px solid pink;
+            p{
+              padding: 1vw;
+              font-size: 17px;
+            }
+            h3{
+              padding: 0vw 1vw;
+              font-size: 15px;
+            }
+            a{
+              padding: 1vw;
+              font-size: 15px;
+            }
+          }
+        }
+        .pacotePrice{
+          margin-top: 10px;
+          width: 100%;
+          height: 20%;
+          display: flex;
+          padding: 10px;
+          justify-content: space-between;          
+          
+          .price{
+            align-items: center;
+            justify-content: center;
+            h2{
+              text-align: center;
+              color: #3a4ad9;
+            }
+          }
+          .pacotepricebuttom{
+            display: flex;
+            align-items: center;
+          }
+        }
+    }
+  }
+
+  .pacotesLists{
+    margin-top: 15px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 90vw;
+    overflow: hidden;
+    
+    .produtosSemelhantes{
+      width: 90vw;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+
+      .cards{
+        margin: 25px auto;
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        gap: 15px;
+
+        .card{
+          
+          h1{
+            font-size: 25px;
+            text-align: center;
+          }
+        }
+      }
+    }
+
+    h2{
+      margin: auto;
+    }
+
+    .produtosRelacionados{
+      display: flex;
+      flex-direction: column;
+      width: 90vw;
+      margin-top: 30px;
+      
+      .posicao{
+        margin: 25px auto;
+        display: flex;
+        gap: 15px;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: center;
+        width: 100%; 
+
+        a{
+          display: flex;
+          flex-direction: row;
+        }
+      }
+    }
+  }
+
+  @media(max-width: 765px){
+
+    .produtoContent{
+      width: 100vw;
+      height: 100%;
+      flex-direction: column;
+      
+    .imgLateral{
+      width: 100%;
+      display: flex;
+      flex-direction: row;
+    }
+    .imgCentral{ 
+      width: 100%;
+      height: 350px;
+    }
+    .produtoInformation{
+      margin: auto;
+      width: 90%;
+    }
+  }
+  }
+`
