ad7a43d841392122c8962f295983d42a1d6a6afa
KevinFMfatec <mirendakevin@gmail.com>
2022-10-07 18:21:54 -0300

    Começo da estilização da Home do admin

--- src/pages/Admin/Home/index.tsx
+++ src/pages/Admin/Home/index.tsx
@@ -1,11 +1,32 @@
 import Nav_Admin from "../../../components/Nav_Admin";
 import { Helmet } from 'react-helmet';
 import * as S from "./styles"
+import Tags from "../../../interfaces/tags";
+import { Link } from "react-router-dom";
 
 function Home() {
     return (
         <>
             <Nav_Admin />
+            <S.Home>
+                <div className="Cards">
+                    <div className="conteudo">
+                        <Link to='/admin/produtos'>produtos</Link>
+                    </div>
+                    <div className="conteudo">
+                        <Link to='/admin/pacotes'>pacotes</Link>
+                    </div>
+                    <div className="conteudo">
+                        <Link to='/admin/ofertas'>ofertas</Link>
+                    </div>
+                    <div className="conteudo">
+                        <Link to='/admin/tags'>tags</Link>
+                    </div>
+                    <div className="conteudo">
+                        <Link to='/admin/categorias'>categorias</Link>
+                    </div>
+                </div>
+            </S.Home>
         </>
     )
 }
--- src/pages/Admin/Home/styles.ts
+++ src/pages/Admin/Home/styles.ts
@@ -4,5 +4,21 @@ export const Home = styled.div`
 
 main{
     display: flex;
+    height: 100%;
 }
-`
+.Cards{
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around; 
+    .conteudo {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 5em;
+        width: 15em;
+        border: 1px solid red;
+        background-color: gray;
+    }
+}
+    `
