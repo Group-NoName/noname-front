f0e1aff6e57d421aa2752439ae653a24117967d7
ZeroPirata <gabriel0gsbn@gmail.com>
2022-10-08 20:05:05 -0300

    Style - espaçamento ajustado

--- src/validators/validadorDesconto.tsx
+++ src/validators/validadorDesconto.tsx
@@ -2,18 +2,16 @@ import React from "react";
 import Iproduto from "../interfaces/produto";
 
 export default class ValidadorDesconto {
-    validar(produto: Iproduto | undefined) {
-        if (produto?.desconto == 0) {
-            return (
-                <h1>R$ {produto?.preco}</h1>
-            )
-        } else if (!(produto?.desconto == 0)) {
-            return (
-                <>
-                    <p>De: R${produto?.preco}</p>
-                    <h1>Por: R${produto?.desconto.toFixed(2)}</h1>
-                </>
-            )
-        }
+  validar(produto: Iproduto | undefined) {
+    if (produto?.desconto == 0) {
+      return <h1>R$ {produto?.preco}</h1>;
+    } else if (!(produto?.desconto == 0)) {
+      return (
+        <>
+          <p>De: R${produto?.preco}</p>
+          <h1>Por: R${produto?.desconto.toFixed(2)}</h1>
+        </>
+      );
     }
-};
+  }
+}
