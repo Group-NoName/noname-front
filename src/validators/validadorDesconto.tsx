28ff441cc28cd3f43cda722d007d6b6a49943684
ZeroPirata <gabriel0gsbn@gmail.com>
2022-10-09 19:30:06 -0300

    Refactor - Ajuste no estilo do validators

--- src/validators/validadorDesconto.tsx
+++ src/validators/validadorDesconto.tsx
@@ -4,13 +4,39 @@ import Iproduto from "../interfaces/produto";
 export default class ValidadorDesconto {
   validar(produto: Iproduto | undefined) {
     if (produto?.desconto == 0) {
-      return <h1>R$ {produto?.preco}</h1>;
+      return (
+        <h1
+          style={{
+            fontSize: "5em",
+            width: "200px",
+            height: "75px",
+          }}
+        >
+          R$ {produto?.preco}
+        </h1>
+      );
     } else if (!(produto?.desconto == 0)) {
       return (
-        <>
-          <p>De: R${produto?.preco}</p>
-          <h1>Por: R${produto?.desconto.toFixed(2)}</h1>
-        </>
+        <div>
+          <p
+            style={{
+              width: "150px",
+              fontSize: "2em",
+            }}
+          >
+            De: R${produto?.preco}
+          </p>
+          <h1
+            style={{
+              width: "350px",
+              fontWeight: "bold",
+              fontSize: "3em",
+              marginTop: "-0.5em"
+            }}
+          >
+            Por: R${produto?.desconto.toFixed(2)}
+          </h1>
+        </div>
       );
     }
   }
