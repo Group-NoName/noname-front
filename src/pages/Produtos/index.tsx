a638ac76ec82edd6d3b92e110b0c182d18d9c989
ZeroPirata <gabriel0gsbn@gmail.com>
2022-10-09 19:34:24 -0300

    Fix - Validação arrumada

--- src/pages/Produtos/index.tsx
+++ src/pages/Produtos/index.tsx
@@ -1,55 +1,57 @@
-import { AxiosError } from 'axios';
-import { useCallback, useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import CardProds from '../../components/CardProds';
-import Nav_ from '../../components/Nav';
-import Iproduto from '../../interfaces/produto';
-import { api } from '../../service/api';
-import * as S from './styles';
+import { AxiosError } from "axios";
+import { useCallback, useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import CardProds from "../../components/CardProds";
+import Nav_ from "../../components/Nav";
+import Iproduto from "../../interfaces/produto";
+import { api } from "../../service/api";
+import * as S from "./styles";
 
 function Produtos() {
+  const [produtos, setProduto] = useState<Iproduto[]>([]);
 
-    const [produtos, setProduto] = useState<Iproduto[]>([])
+  useEffect(() => {
+    getAllProdutos();
+  });
 
-    useEffect(() => { getAllProdutos() })
+  async function getAllProdutos() {
+    const response = await api.get<Iproduto[]>("/produto/produtos");
+    setProduto(response.data);
+  }
 
-    async function getAllProdutos() {
-        const response = await api.get<Iproduto[]>('/produto/produtos')
-        setProduto(response.data)
-    }
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
-
-    return (
-        <>
-            <S.Container>
-                <section>
-                    <header>
-                        <Nav_ />
-                    </header>
-                    <main>
-                        <div className="produtos">
-                            <h1>Produtos</h1>
-                            <div className="produtosmap">
-                                {produtos && produtos.map(i => {
-                                    if (i.desconto == 0) {
-                                        return (
-                                            <CardProds key={i.id} imageURL={`${i.images[0].url}`} name={`${i.nome}`} produtoID={`${i.id}`} preco={i.preco} />
-                                        )
-                                    }
-                                    else {
-                                        return (
-                                            <CardProds key={i.id} imageURL={`${i.images[0].url}`} name={`${i.nome}`} produtoID={`${i.id}`} preco={String(i.desconto.toFixed(2))} />
-                                        )
-                                    }
-                                })}
-                            </div>
-                        </div>
-                    </main>
-                </section>
-            </S.Container>
-        </>
-    )
+  return (
+    <>
+      <S.Container>
+        <section>
+          <header>
+            <Nav_ />
+          </header>
+          <main>
+            <div className="produtos">
+              <h1>Produtos</h1>
+              <div className="produtosmap">
+                {produtos &&
+                  produtos.map((i) => {
+                    return (
+                      <CardProds
+                        key={i.id}
+                        imageURL={`${i.images[0].url}`}
+                        name={`${i.nome}`}
+                        produtoID={`${i.id}`}
+                        preco={i.preco}
+                        produtos={i}
+                      />
+                    );
+                  })}
+              </div>
+            </div>
+          </main>
+        </section>
+      </S.Container>
+    </>
+  );
 }
 
 export default Produtos;
