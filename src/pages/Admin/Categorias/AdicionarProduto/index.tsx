913f813ac6967e41acc4a49a842584c89adffd03
ZeroPirata <gabriel0gsbn@gmail.com>
2022-11-04 22:41:23 -0300

    Feat - Adicionar vários produtos de uma vez em um serviço

---
+++ src/pages/Admin/Categorias/AdicionarProduto/index.tsx
@@ -0,0 +1,128 @@
+import { useState, useCallback, useEffect } from "react";
+import { Button, Form } from "react-bootstrap";
+import { useFieldArray, useForm, Control } from "react-hook-form";
+import { useNavigate, useParams } from "react-router-dom";
+/* import Button from "../../../../components/Button"; */
+import Nav_Admin from "../../../../components/Nav_Admin";
+import ICategoria from "../../../../interfaces/categoria";
+import { api } from "../../../../service/api";
+import useStateView from "../../../../validators/useStateView";
+import * as S from "./styles";
+
+type CadastroProduto = {
+  produtos: {
+    nome: string;
+  }[];
+};
+
+function cadastroProduto() {
+  const navigate = useNavigate();
+  const [status, setStatus] = useState({
+    type: "",
+    mensagem: "",
+  });
+  const stateView = new useStateView();
+
+  const { id } = useParams();
+  const cadastroProduto = useCallback(async (data: CadastroProduto) => {
+    await api
+      .post<CadastroProduto>(`/produto/cadastro/${id}`, data.produtos)
+      .then(function (response) {
+        if (response) {
+          setStatus({
+            type: "sucesso",
+            mensagem: `${response.data}`,
+          }),
+            navigate("/admin/produtos", {
+              state: {
+                data: response.data,
+                status: response.status,
+              },
+            });
+        }
+      })
+      .catch(function (error) {
+        if (error.response) {
+          setStatus({
+            type: "error",
+            mensagem: `${error.response.data}`,
+          });
+        }
+      });
+  }, []);
+
+  const onSubmit = useCallback(async (data: CadastroProduto) => {
+    cadastroProduto(data);
+  }, []);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    control,
+  } = useForm<CadastroProduto>({
+    mode: "onBlur",
+    defaultValues: {
+      produtos: [{ nome: "" }],
+    },
+  });
+
+  const { fields, append, remove } = useFieldArray({
+    name: "produtos",
+    control,
+    rules: {
+      required: "Por favor, adicionar 1 ou vários produtos",
+    },
+  });
+
+  return (
+    <>
+      <S.Cadastro>
+        <section>
+          <header>
+            <Nav_Admin />
+          </header>
+          <main>
+            {stateView.validacao(status.type, status.mensagem)}
+            <div className="Form">
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="nome">
+                  {fields.map((item, index) => {
+                    return (
+                      <>
+                        <label htmlFor="nome">Nome</label>
+                        <input
+                          type="text"
+                          required
+                          placeholder="Produto X"
+                          {...register(`produtos.${index}.nome`)}
+                        />
+                        <button type="button" onClick={() => remove(index)}>
+                          Delete
+                        </button>
+                      </>
+                    );
+                  })}
+                  <button
+                    type="button"
+                    onClick={() => {
+                      append({
+                        nome: "",
+                      });
+                    }}
+                  >
+                    Append
+                  </button>
+                  <Button variant="primary" type="submit">
+                    Submit
+                  </Button>
+                </div>
+              </form>
+            </div>
+          </main>
+        </section>
+      </S.Cadastro>
+    </>
+  );
+}
+export default cadastroProduto;
